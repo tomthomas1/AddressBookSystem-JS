b73d294df6341973bd39b7fb15371747363ab8ce
tomthomas1 <tomthomas8688@gmail.com>
2022-04-08 00:06:47 +0530

    Valid Contacts are added using regex

--- AddressBook.js
+++ AddressBook.js
@@ -1,15 +1,29 @@
 console.log("******* Welcome To Address Book System *******");
+/**
+ * Regex pattern for the data 
+ */
+const nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
+const addressRegex = RegExp('^[a-zA-z0-9#,]{4,}$');
+const cityStateRegex = RegExp('^[a-zA-z]{4,}$');
+const zipRegex = RegExp("^[0-9]{3}\\s{0,1}[0-9]{3}$");
+const phoneNumberRegex = RegExp("^[0-9]{2}[-]{1}[0-9]{10}$");
+const emailRegex = RegExp("^[a-zA-Z]+[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]+$");
 
+/**
+ * Class contact where we have defined the constructor and the getter and setter
+ * We are setting the value if the regex pattern matches.
+ */
 class Contact{
 
     constructor(...params){
         this.firstName = params[0];
         this.lastName = params[1];
-        this.city = params[2];
-        this.state = params[3];
-        this.zip = params[4];
-        this.phoneNumber = params[5];
-        this.email = params[6]
+        this.address = params[2];
+        this.city = params[3];
+        this.state = params[4];
+        this.zip = params[5];
+        this.phoneNumber = params[6];
+        this.email = params[7];
     }
 
     get firstName(){
@@ -18,6 +32,10 @@ class Contact{
 
     get lastName(){
         return this._lastName;
+    }
+
+    get address(){
+        return this._address;
     }
 
     get city(){
@@ -41,37 +59,70 @@ class Contact{
     }
 
     set firstName(firstName){
-        this._firstName = firstName;
+        if (nameRegex.test(firstName))
+            this._firstName = firstName;
+        else
+            throw "**** FIRST NAME is Incorrect ****";
     }
 
     set lastName(lastName){
-        this._lastName = lastName;
+        if (nameRegex.test(lastName))
+            this._lastName = lastName;
+        else
+            throw "**** LAST NAME is Incorrect ****";
+    }
+
+    set address(address){
+        if (addressRegex.test(address))
+            this._address = address;
+        else
+            throw "**** ADDRESS is Incorrect ****";
     }
 
     set city(city){
-        this._city = city;
+        if (cityStateRegex.test(city))
+            this._city = city;
+        else
+            throw "**** CITY is Incorrect ****";
     }
 
     set state(state){
-        this._state = state;
+        if (cityStateRegex.test(state))
+            this._state = state;
+        else
+            throw "**** STATE is Incorrect ****";
     }
 
     set zip(zip){
-        this._zip = zip;
+        if (zipRegex.test(zip))
+            this._zip = zip;
+        else
+            throw "**** ZIP is Incorrect ****";
     }
 
     set phoneNumber(phoneNumber){
-        this._phoneNumber = phoneNumber;
+        if (phoneNumberRegex.test(phoneNumber))
+            this._phoneNumber = phoneNumber;
+        else
+            throw "**** PHONE NUMBER is Incorrect ****";
     }
 
     set email(email){
-        this._email = email;
+        if (emailRegex.test(email))
+            this._email = email;
+        else
+            throw "**** EMAIL ADDRESS is Incorrect ****";
     }
 
     toString(){
-        return "First Name : "+ this.firstName + ", Last Name : "+ this.lastName + ", City : "+ this.city + ", State : "+ this.state +", Zip : "+ this.zip+ ", Phone Number : "+ this.phoneNumber + ", Email : "+ this.email;
+        return "First Name : "+ this.firstName + ", Last Name : "+ this.lastName + ", Address : " + this.address + ", City : "+ this.city + ", State : "+ this.state +", Zip : "+ this.zip+ ", Phone Number : "+ this.phoneNumber + ", Email : "+ this.email;
     }
 }
 
-let contact = new Contact("Tom", "Thomas", "Pune", "MH", 411062, 99999999, "tom@gmail.com");
-console.log(contact.toString());
+try{
+    let contact = new Contact("Tom", "Thomas", "Sparkle", "Pune", "Maharashtra", 411062, "91-9999999999", "tom@gmail.com");
+    console.log(contact.toString());
+}
+catch(e){
+    console.log(e);
+}
